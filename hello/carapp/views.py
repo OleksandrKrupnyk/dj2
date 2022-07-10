2ab45bd257628e1b39a09d8926e0b1f2e59e3090
A <u@u.uz>
2022-07-10 14:40:36 +0300

    templates2

--- hello/carapp/views.py
+++ hello/carapp/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
 from django.template.response import TemplateResponse
 from django.http import \
-    HttpResponse, HttpResponseRedirect, HttpResponsePermanentRedirect
+    HttpResponse, HttpResponseRedirect, HttpResponsePermanentRedirect, HttpRequest
+#from django.shortcuts import render
 
 
 # Create your views here.
-def index(request: object) -> object:
+def index(request: HttpRequest) -> object:
     return TemplateResponse(request, "carapp\\home.html")
     # header = "Персональні дані" #Звичайна змінна
     # langs = ["Англійська","Німецька","Іспанська"], #масив
@@ -15,35 +15,35 @@ def index(request: object) -> object:
     # return TemplateResponse(request, "carapp\\index_app1.html", context=data)
 
 
-def about(request: object) -> object:
+def about(request: HttpRequest) -> HttpResponse:
     return HttpResponse("<h1>Про нас</h1>")
 
 
-def contacts(request: object) -> object:
+def contacts(request: HttpRequest) -> HttpResponse:
     return HttpResponse("<h1>Контакти</h1>")
 
 
-def users1(request: object, id: int = 1, name: str = "Олександр") -> object:
+def users1(request: HttpRequest, id: int = 1, name: str = "Олександр") -> HttpResponse:
     output = "<h2>Користувач:</h2><h3>id: {0} Ім'я: {1}</h3>".format(id, name)
     return HttpResponse(output)
 
 
-def products(request: object, productid: int = 1) -> object:
+def products(request: HttpRequest, productid: int = 1) -> HttpResponse:
     cat = request.GET.get("cat", "")
     output = "<h2>Товар №{0}</h2><br>Категорія: {1}".format(productid, cat)
     return HttpResponse(output)
 
 
-def users(request: object) -> object:
+def users(request: HttpRequest) -> HttpResponse:
     id = request.GET.get("id", 1)
     name = request.GET.get("name", "Олександр")
     output = "<h2>Користувач:</h2><h3>id: {0} Ім'я: {1}</h3>".format(id, name)
     return HttpResponse(output)
 
 
-def personas(request):
+def personas(request: HttpRequest) -> HttpResponseRedirect:
     return HttpResponseRedirect("/about")
 
 
-def details(request):
+def details(request: HttpRequest) -> HttpResponsePermanentRedirect:
     return HttpResponsePermanentRedirect("/")
